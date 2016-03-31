b34e102205010069102ea3d44b5bf23ae16100bb
jeradjohnson <jeradjohnsonp@gmail.com>
2016-03-31 11:02:39 -0500

    Update technology.lua

--- prototypes/technology.lua
+++ prototypes/technology.lua
@@ -1,33 +1,74 @@
 data:extend({
   {
       type = "technology",
-      name = "Advanced Oil Pumps",
+      name = "AdvancedOilPumpTech1",
       icon = "__base__/graphics/technology/oil-gathering.png",
       effects =
       {
         {
             type = "unlock-recipe",
             recipe = "AOP-pumpjack-1"
-        },
-        {
-            type = "unlock-recipe",
-            recipe = "AOP-pumpjack-2"
-        },
-		{
-            type = "unlock-recipe",
-            recipe = "AOP-pumpjack-3"
         }
       },
       prerequisites = {"oil-processing"},
       unit =
       {
-        count = 1,
+        count = 50,
         ingredients =
         {
           {"science-pack-1", 1},
           {"science-pack-2", 1}
         },
-        time = 5
+        time = 15
+      }
+  },
+  {
+      type = "technology",
+      name = "AdvancedOilPumpTech2",
+      icon = "__base__/graphics/technology/oil-gathering.png",
+      effects =
+      {
+        {
+            type = "unlock-recipe",
+            recipe = "AOP-pumpjack-2"
+        }
+      },
+      prerequisites = {"AdvancedOilPumpTech1"},
+      unit =
+      {
+        count = 100,
+        ingredients =
+        {
+          {"science-pack-1", 1},
+          {"science-pack-2", 1},
+		  {"science-pack-3", 1}
+        },
+        time = 30
+      }
+  },
+  {
+      type = "technology",
+      name = "AdvancedOilPumpTech3",
+      icon = "__base__/graphics/technology/oil-gathering.png",
+      effects =
+      {
+		{
+            type = "unlock-recipe",
+            recipe = "AOP-pumpjack-3"
+        }
+      },
+      prerequisites = {"AdvancedOilPumpTech2"},
+      unit =
+      {
+        count = 250,
+        ingredients =
+        {
+          {"science-pack-1", 1},
+          {"science-pack-2", 1},
+		  {"science-pack-3", 1},
+		  {"alien-science-pack", 2}
+        },
+        time = 30
       }
   }
 })
